7f3e44aa76e534bf619ac7cbb24a7df11007a2af
Antonio Murgia <tmnd91@gmail.com>
2015-04-28 10:09:31 +0200

    final version

--- src/main/scala/org/openstack/clients/ceilometer/v2/CeilometerClient.scala
+++ src/main/scala/org/openstack/clients/ceilometer/v2/CeilometerClient.scala
@@ -320,13 +320,13 @@ class JettyCeilometerClient(ceilometerUrl : URL,
  * Implementation of the flyweight pattern to get a CeilometerClient
  */
 object CeilometerClient{
-  private val instances : scala.collection.mutable.Map[Int,ICeilometerClient2] = scala.collection.mutable.Map()
+  private val instances : scala.collection.mutable.Map[Int,ICeilometerClient] = scala.collection.mutable.Map()
   def getInstance(ceilometerUrl : URL, keystoneUrl : URL, tenantName : String,  username : String, password : String, connectTimeout: Int, readTimeout: Int) = {
     this.synchronized{
       val hashcode = getHashCode(ceilometerUrl,keystoneUrl,tenantName,username,password)
       if (!instances.contains(hashCode))
-        //instances(hashCode) = new JettyCeilometerClient(ceilometerUrl, keystoneUrl, tenantName,  username, password, connectTimeout, readTimeout)
-        instances(hashCode) = new DispatchCeilometerClient(ceilometerUrl, keystoneUrl, tenantName,  username, password, connectTimeout, readTimeout)
+        instances(hashCode) = new JettyCeilometerClient(ceilometerUrl, keystoneUrl, tenantName,  username, password, connectTimeout, readTimeout)
+        //instances(hashCode) = new DispatchCeilometerClient(ceilometerUrl, keystoneUrl, tenantName,  username, password, connectTimeout, readTimeout)
     }
     instances(hashCode)
   }
